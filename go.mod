82c8731fccbe9fb20beef3b9e16dcebd451ff863
cheerioskun <hpandey110301@gmail.com>
2025-07-21 23:40:06 +0530

    Complete ShortHandler. Add metrics updation and inmem mapping from short to long URL

--- go.mod
+++ go.mod
@@ -3,13 +3,16 @@ module github.com/cheerioskun/url-shortener-infracloud
 go 1.23.1
 
 require (
+	github.com/gin-gonic/gin v1.10.1
+	golang.org/x/crypto v0.40.0
+)
+
+require (
 	github.com/bytedance/sonic v1.13.3 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
-	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
-	github.com/gin-gonic/gin v1.10.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
@@ -24,7 +27,6 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
 	golang.org/x/arch v0.19.0 // indirect
-	golang.org/x/crypto v0.40.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log/slog"
 	"net/http"
 	"net/url"
+	"sync"
+	"sync/atomic"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/blake2b"
@@ -13,14 +15,22 @@ import (
 
 const ServerListenAddr = "localhost:3000"
 
+var db sync.Map
+var metrics map[string]*atomic.Int64 = make(map[string]*atomic.Int64)
+
 func main() {
+	r := setupRouter()
+	r.Run(ServerListenAddr)
+}
+
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(ServerListenAddr)
+	return r
 }
 
 type ShortRequest struct {
@@ -45,18 +55,29 @@ func ShortHandler(c *gin.Context) {
 		return
 	}
 	slog.Info("URL parsed successfully", "url", req.URL, "domain", longURL.Host, "path", longURL.Path)
-	// Hash it!
+
+	// Hash it and encode to base64
 	hasher, _ := blake2b.New256([]byte("Hey InfraCloud!"))
 	_, _ = hasher.Write([]byte(req.URL))
 	hash := hasher.Sum(nil)
-	// Encode it base64
 	encodedStr := base64.URLEncoding.EncodeToString(hash)
+
 	// Form new URL
 	newURL := fmt.Sprintf("%s/long/%s", ServerListenAddr, encodedStr)
+	// Save in db/map
+	db.Store(encodedStr, req.URL)
+	slog.Debug("new URL mapping stored", "short", newURL, "long", req.URL)
+
 	c.IndentedJSON(http.StatusOK, ShortResponse{
 		URL: newURL,
 	})
-	// TODO: Update metrics
-	// TODO: Store mapping from hash to long URL
 
+	// Triggering an async routine, there is no chance of this getting stuck in a deadlock,
+	// So no need to wrap a context and timeout. This should not cause goroutine leak.
+	go metrics[longURL.Host].Add(1)
+
+}
+
+func LongHandler(c *gin.Context) {
+	// Check for valid shortURL
 }
